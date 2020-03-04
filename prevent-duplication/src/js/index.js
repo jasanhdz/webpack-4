0ff16f8fe3275efbad4ac0c558c3df79eeac677b
Jasan Hernandez <jasan_azael@hotmail.es>
2020-03-04 10:34:51 -0600

    prevent-duplication use optimization to splitChunks

---
+++ prevent-duplication/src/js/index.js
@@ -0,0 +1,5 @@
+import "../css/index.css";
+import { render } from "react-dom";
+import React from "react";
+import App from "./components/App";
+render(<App />, document.getElementById("app"));
---
+++ prevent-duplication/webpack.dev.config.js
@@ -0,0 +1,81 @@
+const path = require("path");
+const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    home: path.resolve(__dirname, "src/js/index.js"),
+    contact: path.resolve(__dirname, "src/js/contact.js")
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].js"
+  },
+  devServer: {
+    hot: true,
+    open: true,
+    port: 9000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader", // luego quiero que los inyectes a mi html
+          {
+            loader: "css-loader", // quiero que primero interpretes que pasa con los imports
+            options: {
+              importLoaders: 1
+            }
+          },
+          "postcss-loader"
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: ["style-loader", "css-loader", "less-loader"]
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.styl$/,
+        use: ["style-loader", "css-loader", "stylus-loader"]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        use: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(jpg|png|gif|woff|eot|ttf|svg|mp4|webm)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 90000
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      title: "webpack-dev-server",
+      template: path.resolve(__dirname, "index.html")
+    })
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+      minSize: 0,
+      name: "commons"
+    }
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  }
+};
